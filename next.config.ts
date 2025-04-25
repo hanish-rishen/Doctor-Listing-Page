c96dd5e98492ecf474ca57d36137db947aeca52e
Hanish Rishen <110720727+hanish-rishen@users.noreply.github.com>
2025-04-25 17:14:37 +0530

    chore: acceptable domains added for images

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: [
+      "doctorlistingingestionpr.azureedge.net",
+      "doctorlistingingestionpr.blob.core.windows.net",
+    ],
+  },
 };
 
 export default nextConfig;
